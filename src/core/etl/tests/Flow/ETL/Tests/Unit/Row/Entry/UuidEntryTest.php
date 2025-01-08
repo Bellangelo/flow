dffc47b90f4a39ff4755a849a8583e6c3ca1b916
Bellangelo <aggelosbellos7@gmail.com>
2025-01-08 02:00:42 +0200

    Fix UuidEntryTest

--- src/core/etl/tests/Flow/ETL/Tests/Unit/Row/Entry/UuidEntryTest.php
+++ src/core/etl/tests/Flow/ETL/Tests/Unit/Row/Entry/UuidEntryTest.php
@@ -55,7 +55,7 @@ final class UuidEntryTest extends FlowTestCase
     {
         $entry = UuidEntry::from('entry-name', $value);
 
-        self::assertEquals($value, $entry->value()->toString());
+        self::assertEquals($value, $entry->value()?->toString());
     }
 
     #[DataProvider('is_equal_data_provider')]
@@ -95,6 +95,6 @@ final class UuidEntryTest extends FlowTestCase
         $newEntry = $entry->rename('new-entry-name');
 
         self::assertEquals('new-entry-name', $newEntry->name());
-        self::assertEquals($uuid->toString(), $newEntry->value()->toString());
+        self::assertEquals($uuid->toString(), $newEntry->value()?->toString());
     }
 }
